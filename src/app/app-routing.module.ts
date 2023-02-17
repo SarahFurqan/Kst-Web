33101aa16cbc2397be74dec794723c6e402b13da
asrar ahmed <asrar.ahmed@ks-technologies.net>
2023-02-17 12:13:28 +0500

    routing and new components arranged files over application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,46 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Components/Pages/home/home.component';
+import { AboutUsComponent } from './Components/Pages/about-us/about-us.component';
+import { OurServicesComponent } from './Components/Pages/our-services/our-services.component';
+import { BlogNewsComponent } from './Components/Pages/blog-news/blog-news.component';
+import { PortfolioComponent } from './Components/Pages/portfolio/portfolio.component';
+import { JobsComponent } from './Components/Pages/jobs/jobs.component';
+import { ContactUsComponent } from './Components/Pages/contact-us/contact-us.component';
+import { LogInPageComponent } from './Components/AuthForms/log-in-page/log-in-page.component';
+import { SignUpPageComponent } from './Components/AuthForms/sign-up-page/sign-up-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',   redirectTo: 'home', pathMatch: 'full' },
+  { component: HomeComponent,
+    path:"home"  
+  },
+  { component: AboutUsComponent,
+    path: 'about'
+  },
+  { component: OurServicesComponent,
+    path:"services"  
+  },
+  { component: BlogNewsComponent,
+    path:"blog&news"  
+  },
+  { component: PortfolioComponent,
+    path:"portfolio"  
+  },
+  { component: JobsComponent,
+    path:"jobs"  
+  },
+  { component: ContactUsComponent,
+    path:"contact"  
+  },
+  { component: LogInPageComponent,
+    path: 'login'
+  },
+  { component: SignUpPageComponent,
+    path: 'signup'
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RegisterComponent } from './Components/register/register.component';
-import { DashboardComponent } from './Components/dashboard/dashboard.component';
-import { ButtonComponent } from './Components/commonComponents/button/button.component';
-import { InputComponent } from './Components/commonComponents/input/input.component';
-import { HeaderComponent } from './Components/header/header.component';
-import { FooterComponent } from './Components/footer/footer.component';
-import { HomeComponent } from './Components/home/home.component';
+import { RegisterComponent } from './Components/AuthForms/register/register.component';
+import { DashboardComponent } from './Components/MainDashbordComponents/Dashboard/dashboard.component';
+import { ButtonComponent } from './Components/GenericComponents/button/button.component';
+import { InputComponent } from './Components/GenericComponents/input/input.component';
+import { HeaderComponent } from './Components/MainDashbordComponents/header/header.component';
+import { FooterComponent } from './Components/MainDashbordComponents/footer/footer.component';
+import { HomeComponent } from './Components/Pages/home/home.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { LoginComponent } from './Components/login/login.component';
-import { GenericInputComponent } from './Components/commonComponents/generic-input/generic-input.component';
-import { PasswordInputComponent } from './Components/commonComponents/password-input/password-input.component';
-import { SignUpPageComponent } from './Components/sign-up-page/sign-up-page.component';
-import { LogInPageComponent } from './Components/log-in-page/log-in-page.component';
+import { LoginComponent } from './Components/AuthForms/login/login.component';
+import { GenericInputComponent } from './Components/GenericComponents/generic-input/generic-input.component';
+import { PasswordInputComponent } from './Components/GenericComponents/password-input/password-input.component';
+import { SignUpPageComponent } from './Components/AuthForms/sign-up-page/sign-up-page.component';
+import { LogInPageComponent } from './Components/AuthForms/log-in-page/log-in-page.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AboutUsComponent } from './Components/Pages/about-us/about-us.component';
+import { ContactUsComponent } from './Components/Pages/contact-us/contact-us.component';
+import { OurServicesComponent } from './Components/Pages/our-services/our-services.component';
+import { BlogNewsComponent } from './Components/Pages/blog-news/blog-news.component';
+import { PortfolioComponent } from './Components/Pages/portfolio/portfolio.component';
+import { JobsComponent } from './Components/Pages/jobs/jobs.component';
+
 
 
 @NgModule({
@@ -32,7 +39,13 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     GenericInputComponent,
     PasswordInputComponent,
     SignUpPageComponent,
-    LogInPageComponent
+    LogInPageComponent,
+    AboutUsComponent,
+    ContactUsComponent,
+    OurServicesComponent,
+    BlogNewsComponent,
+    PortfolioComponent,
+    JobsComponent
   ],
   imports: [
     BrowserModule,
