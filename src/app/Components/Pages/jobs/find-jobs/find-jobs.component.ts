d99115a99902f726402507278b931ee16bce3582
Mughees Ahmed <mughees.ahmed@ks-technologies.net>
2023-02-21 19:05:29 +0500

    jobs page design

--- src/app/Components/Pages/jobs/find-jobs/find-jobs.component.ts
+++ src/app/Components/Pages/jobs/find-jobs/find-jobs.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+import { faMagnifyingGlass,faLocationDot } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-find-jobs',
   templateUrl: './find-jobs.component.html',
   styleUrls: ['./find-jobs.component.css']
 })
 export class FindJobsComponent {
-
+  jobs = faMagnifyingGlass;
+  locate = faLocationDot;
 }
