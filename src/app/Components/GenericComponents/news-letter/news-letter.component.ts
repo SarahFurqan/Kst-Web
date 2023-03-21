0e8d1933cb1fceea187a152b71048d12f023d6c2
asrar ahmed <asrar.ahmed@ks-technologies.net>
2023-03-21 19:26:59 +0500

    api and validation for news letter

--- src/app/Components/GenericComponents/news-letter/news-letter.component.ts
+++ src/app/Components/GenericComponents/news-letter/news-letter.component.ts
@@ -12,15 +12,17 @@ export class NewsLetterComponent {
   constructor(private http: HttpClient) {}
 
   subscribe() {
-    const apiUrl = environment.baseUrl+"contact";
+    const apiUrl = environment.baseUrl+"subscribe";
     const headers = new HttpHeaders({
       "Content-Type": "application/json",
       "Accept": "application/json"
     });
     console.log(this.email)
+    this.email = ''; 
     this.http.post(apiUrl, { email: this.email }).subscribe(
       (response) => {
         console.log('Subscribed successfully');
+        this.email = '';
       },
       (error) => {
         console.log('Subscription failed');
