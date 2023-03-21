dd45eabeb67c4aef3f6dbc68bc6b5f6b0199e417
asrar ahmed <asrar.ahmed@ks-technologies.net>
2023-03-21 18:52:05 +0500

    news letter api integrated

--- src/app/Components/GenericComponents/news-letter/news-letter.component.ts
+++ src/app/Components/GenericComponents/news-letter/news-letter.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environment/environment';
 @Component({
   selector: 'app-news-letter',
   templateUrl: './news-letter.component.html',
   styleUrls: ['./news-letter.component.css']
 })
 export class NewsLetterComponent {
+  email!: string;
 
+  constructor(private http: HttpClient) {}
+
+  subscribe() {
+    const apiUrl = environment.baseUrl+"contact";
+    const headers = new HttpHeaders({
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    });
+    console.log(this.email)
+    this.http.post(apiUrl, { email: this.email }).subscribe(
+      (response) => {
+        console.log('Subscribed successfully');
+      },
+      (error) => {
+        console.log('Subscription failed');
+      }
+    );
+  }
 }
