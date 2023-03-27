6b54a94b9774f1419fc624ef8a78ea9733983986
asrar ahmed <asrar.ahmed@ks-technologies.net>
2023-03-27 10:38:25 +0500

    deleted files, error page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RegisterComponent } from './Components/AuthForms/register/register.component';
 import { DashboardComponent } from './Components/MainDashbordComponents/Dashboard/dashboard.component';
-import { ButtonComponent } from './Components/GenericComponents/button/button.component';
 import { HeaderComponent } from './Components/MainDashbordComponents/header/header.component';
 import { FooterComponent } from './Components/MainDashbordComponents/footer/footer.component';
 import { HomeComponent } from './Components/Pages/home/home.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { LoginComponent } from './Components/AuthForms/login/login.component';
-import { GenericInputComponent } from './Components/GenericComponents/generic-input/generic-input.component';
-import { SignUpPageComponent } from './Components/AuthForms/sign-up-page/sign-up-page.component';
-import { LogInPageComponent } from './Components/AuthForms/log-in-page/log-in-page.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AboutUsComponent } from './Components/Pages/about-us/about-us.component';
 import { ContactUsComponent } from './Components/Pages/contact-us/contact-us.component';
@@ -49,7 +43,6 @@ import { LandingPageContactUsComponent } from './Components/Pages/contact-us/lan
 import { ContactUsFormPageComponent } from './Components/Pages/contact-us/contact-us-form-page/contact-us-form-page.component';
 import { PrivacyPolicyComponent } from './Components/Pages/privacy-policy/privacy-policy.component';
 import { HttpClientModule } from '@angular/common/http';
-import { UnderConsComponent } from './Components/Pages/under-cons/under-cons.component';
 import { JobsPageComponent } from './Components/AuthForms/jobs-page/jobs-page.component';
 import { ToastrModule } from 'ngx-toastr';
 
@@ -57,16 +50,10 @@ import { ToastrModule } from 'ngx-toastr';
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent,
     DashboardComponent,
-    ButtonComponent,
     HeaderComponent,
     FooterComponent,
     HomeComponent,
-    LoginComponent,
-    GenericInputComponent,
-    SignUpPageComponent,
-    LogInPageComponent,
     AboutUsComponent,
     ContactUsComponent,
     OurServicesComponent,
@@ -99,7 +86,6 @@ import { ToastrModule } from 'ngx-toastr';
     LandingPageContactUsComponent,
     ContactUsFormPageComponent,
     PrivacyPolicyComponent,
-    UnderConsComponent,
     JobsPageComponent
   ],
   imports: [
